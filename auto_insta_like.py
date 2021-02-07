b222ecf260dfba43ecf61e2523aa85399c908ad0
labdhi05 <61301997+labdhi05@users.noreply.github.com>
2021-02-07 16:50:30 +0530

    Add files via upload

---
+++ auto_insta_like.py
@@ -0,0 +1,19 @@
+import time
+import pyautogui
+
+# Give some time before it runs this file
+time.sleep(5)
+
+# Finding Mouse's current position on the screen
+# print(pyautogui.position())
+
+# the instagram is located at x=470, y=586
+
+pyautogui.moveTo(470, 586)
+# pyautogui.scroll(-1200)
+for i in range(5):
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.scroll(-1200)
+
